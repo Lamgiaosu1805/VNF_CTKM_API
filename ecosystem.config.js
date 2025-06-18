0686c9bbb15cfa4dd2b51cc34c812963b1bc6d45
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-06-18 10:58:09 +0700

    add config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+        {
+            name: 'CTKM_TIKLUY',
+            script: './index.js',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+        },
+    ],
+};
